591236e4d210dde5fd004cb1a4f5e12139c075fe
Anastasiia Boreiko <kravchenkoanastasiia92@gmail.com>
2025-08-29 11:22:11 +0300

    TS-Figures_Classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,82 @@
-export interface Figure {}
+export interface Figure {
+  shape: 'triangle' | 'circle' | 'rectangle';
+  color: 'red' | 'green' | 'blue';
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+function assertPositiveSides(...nums: number[]): void {
+  if (!nums.every((n) => n > 0)) {
+    throw new Error('All length should be >= 0');
+  }
+}
 
-export class Circle implements Figure {}
+export class Triangle implements Figure {
+  public shape: 'triangle' = 'triangle';
 
-export class Rectangle implements Figure {}
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    private a: number,
+    private b: number,
+    private c: number,
+  ) {
+    assertPositiveSides(a, b, c);
 
-export function getInfo(figure): string {
-  return typeof figure;
+    const longest = Math.max(a, b, c);
+    const sum = a + b + c;
+
+    if (2 * longest >= sum) {
+      throw new Error('This is not a Triangle');
+    }
+  }
+
+  getArea(): number {
+    const s = (this.a + this.b + this.c) / 2;
+    const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Circle implements Figure {
+  public shape: 'circle' = 'circle';
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    private radius: number,
+  ) {
+    assertPositiveSides(radius);
+  }
+
+  getArea(): number {
+    const area: number = Math.PI * this.radius * this.radius;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export class Rectangle implements Figure {
+  public shape: 'rectangle' = 'rectangle';
+
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    public color: 'red' | 'green' | 'blue',
+    private width: number,
+    private height: number,
+  ) {
+    assertPositiveSides(width, height);
+  }
+
+  getArea(): number {
+    const area: number = this.width * this.height;
+
+    return Math.floor(area * 100) / 100;
+  }
+}
+
+export function getInfo(figure: Figure): string {
+  const result = `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
+
+  return result;
 }
